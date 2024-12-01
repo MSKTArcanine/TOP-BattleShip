6415fcdf770e170712d0b4ad77e2dbccf8f35b3e
MSKTArcanine <marcantoine1999@live.fr>
2024-12-01 07:14:12 +0100

    change : index.js, to make it fit new interface

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import SubmarineFactory from "./ShipFactory/submarineFactory";
 import CroiseurFactory from "./ShipFactory/croiseurFactory";
 import TorpilleurFactory from "./ShipFactory/torpilleurFactory";
 import CuirasseFactory from "./ShipFactory/cuirasseFactory";
+import SunkEvent from "./PubSub/SunkEvent";
+import HitEvent from "./PubSub/HitEvent";
 
+const sunkEvent = new SunkEvent();
+const hitEvent = new HitEvent()
 const gameBoard = new GameBoard();
-const submarineFactory = new SubmarineFactory(1);
-const croiseurFactory = new CroiseurFactory(1);
-const cuirasseFactory = new CuirasseFactory(1);
-const torpilleurFactory = new TorpilleurFactory(1);
+const submarineFactory = new SubmarineFactory(sunkEvent, hitEvent, 1);
+const croiseurFactory = new CroiseurFactory(sunkEvent, hitEvent, 1);
+const cuirasseFactory = new CuirasseFactory(sunkEvent, hitEvent, 1);
+const torpilleurFactory = new TorpilleurFactory(sunkEvent, hitEvent, 1);
 const factories = [submarineFactory, croiseurFactory, cuirasseFactory, torpilleurFactory]
 
 while(true){
@@ -21,4 +25,9 @@ while(true){
 }
 
 gameBoard.placeShipOnBoard()
+console.log(gameBoard.getBoard())
+gameBoard.receiveAttack(1,1);
+gameBoard.receiveAttack(2,2);
+gameBoard.receiveAttack(0,2);
+gameBoard.receiveAttack(9,9);
 console.log(gameBoard.getBoard())
